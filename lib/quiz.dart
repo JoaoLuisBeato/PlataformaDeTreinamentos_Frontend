0ee007cc56f3ef6e9754c82ffd865fef5b8212ea
Mateus José <mjwf@uol.com.br>
2023-05-19 00:46:26 -0300

    Adição de campo para criar pergunta em cada questão

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -6,19 +6,44 @@ class QuizCall extends StatefulWidget {
 }
 
 class Quiz extends State<QuizCall> {
-  List<String> items = ['Item 1', 'Item 2', 'Item 3'];
+  List<Questions> items = [];
 
   void addItem() {
     setState(() {
-      int itemCount = items.length + 1;
-      items.add('Item $itemCount');
+      int questionCount = items.length + 1;
+      items.add(
+          Questions(questao: 'Questão $questionCount', pergunta: pergunta));
     });
   }
 
+  List<bool> checkboxesParaQuestoes = List.generate(5, (_) => false);
+
   TextStyle style = const TextStyle(fontFamily: 'Nunito', fontSize: 20.9);
+  String pergunta = '';
+
+  int questionCounter = 0;
 
   @override
   Widget build(BuildContext context) {
+
+    final questionField = SizedBox(
+      width: 600,
+      child: TextField(
+        onChanged: (text) {
+          pergunta = text;
+        },
+        keyboardType: TextInputType.multiline,
+        maxLines: null,
+        obscureText: false,
+        style: style,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Pergunta da questão",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Criar QUIZ'),
@@ -27,14 +52,37 @@ class Quiz extends State<QuizCall> {
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(items[index]),
+          return Column(
+            children: [
+              ListTile(
+                title: Text(items[index].questao, style: style),
+              ),
+              questionField,
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20.0),
+                child: Divider(
+                  color: Colors.amber,
+                  height: 2.0,
+                ),
+              ),
+            ],
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: addItem,
+        onPressed: (){
+          addItem();
+          questionCounter++;
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
+}
+
+class Questions {
+  String questao;
+  String pergunta;
+
+  Questions({required this.questao, required this.pergunta});
 }
