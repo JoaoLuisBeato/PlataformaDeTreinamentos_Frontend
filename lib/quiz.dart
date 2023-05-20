59c75ca68b18951408731b6e4b414bcc4930973c
Mateus José <mjwf@uol.com.br>
2023-05-20 17:59:50 -0300

    Quiz finalizado com 4 questões
    esperar rework

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -23,6 +23,12 @@ class Quiz extends State<QuizCall> {
       itemsRespostas.add(Answers(
         respostaDaAlternativaA: respostaA,
         alternativaA: checkAlternativaA,
+        respostaDaAlternativaB: respostaB,
+        alternativaB: checkAlternativaB,
+        respostaDaAlternativaC: respostaC,
+        alternativaC: checkAlternativaC,
+        respostaDaAlternativaD: respostaD,
+        alternativaD: checkAlternativaD,
       ));
     });
   }
@@ -32,8 +38,14 @@ class Quiz extends State<QuizCall> {
   String pergunta = '';
 
   String respostaA = '';
+  String respostaB = '';
+  String respostaC = '';
+  String respostaD = '';
 
   bool checkAlternativaA = false;
+  bool checkAlternativaB = false;
+  bool checkAlternativaC = false;
+  bool checkAlternativaD = false;
 
   int questionCounter = 0;
 
@@ -58,6 +70,12 @@ class Quiz extends State<QuizCall> {
     );
 
     Column returnCheckbox(index, listAnswers) {
+
+      checkAlternativaA = false;
+      checkAlternativaB = false;
+      checkAlternativaC = false;
+      checkAlternativaD = false;
+
       return Column(
         children: [
           CheckboxListTile(
@@ -68,6 +86,9 @@ class Quiz extends State<QuizCall> {
               setState(() {
                 listAnswers[index].alternativaA = value!;
                 checkAlternativaA = listAnswers[index].alternativaA;
+                listAnswers[index].alternativaB = false;
+                listAnswers[index].alternativaC = false;
+                listAnswers[index].alternativaD = false;
               });
             },
             title: SizedBox(
@@ -83,6 +104,102 @@ class Quiz extends State<QuizCall> {
                 decoration: InputDecoration(
                   contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
                   hintText: "Resposta da alternativa A",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              ),
+            ),
+          ),
+          CheckboxListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 420, vertical: 5),
+            value: listAnswers[index].alternativaB,
+            onChanged: (bool? value) {
+              setState(() {
+                listAnswers[index].alternativaB = value!;
+                checkAlternativaB = listAnswers[index].alternativaB;
+                listAnswers[index].alternativaA = false;
+                listAnswers[index].alternativaC = false;
+                listAnswers[index].alternativaD = false;
+              });
+            },
+            title: SizedBox(
+              child: TextField(
+                onChanged: (text) {
+                  listAnswers[index].respostaDaAlternativaB = text;
+                  respostaB = listAnswers[index].respostaDaAlternativaB;
+                },
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                obscureText: false,
+                style: style,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                  hintText: "Resposta da alternativa B",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              ),
+            ),
+          ),
+          CheckboxListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 420, vertical: 5),
+            value: listAnswers[index].alternativaC,
+            onChanged: (bool? value) {
+              setState(() {
+                listAnswers[index].alternativaC = value!;
+                checkAlternativaC = listAnswers[index].alternativaC;
+                listAnswers[index].alternativaA = false;
+                listAnswers[index].alternativaB = false;
+                listAnswers[index].alternativaD = false;
+              });
+            },
+            title: SizedBox(
+              child: TextField(
+                onChanged: (text) {
+                  listAnswers[index].respostaDaAlternativaC = text;
+                  respostaC = listAnswers[index].respostaDaAlternativaC;
+                },
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                obscureText: false,
+                style: style,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                  hintText: "Resposta da alternativa C",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+              ),
+            ),
+          ),
+          CheckboxListTile(
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 420, vertical: 5),
+            value: listAnswers[index].alternativaD,
+            onChanged: (bool? value) {
+              setState(() {
+                listAnswers[index].alternativaD = value!;
+                checkAlternativaD = listAnswers[index].alternativaD;
+                listAnswers[index].alternativaA = false;
+                listAnswers[index].alternativaB = false;
+                listAnswers[index].alternativaC = false;
+              });
+            },
+            title: SizedBox(
+              child: TextField(
+                onChanged: (text) {
+                  listAnswers[index].respostaDaAlternativaD = text;
+                  respostaD = listAnswers[index].respostaDaAlternativaD;
+                },
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                obscureText: false,
+                style: style,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.fromLTRB(20, 15, 20, 15),
+                  hintText: "Resposta da alternativa D",
                   border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(20)),
                 ),
@@ -108,6 +225,14 @@ class Quiz extends State<QuizCall> {
               ),
               questionField,
               returnCheckbox(index, itemsRespostas),
+              Text(itemsRespostas[index].respostaDaAlternativaA),
+              Text(itemsRespostas[index].respostaDaAlternativaB),
+              Text(itemsRespostas[index].respostaDaAlternativaC),
+              Text(itemsRespostas[index].respostaDaAlternativaD),
+              Text(itemsRespostas[index].alternativaA.toString()),
+              Text(itemsRespostas[index].alternativaB.toString()),
+              Text(itemsRespostas[index].alternativaC.toString()),
+              Text(itemsRespostas[index].alternativaD.toString()),
               const Padding(
                 padding: EdgeInsets.symmetric(vertical: 20.0),
                 child: Divider(
@@ -140,14 +265,22 @@ class Questions {
 
 class Answers {
   String respostaDaAlternativaA;
-  //String respostaDaAlternativaB;
-  /*String respostaDaAlternativaC;
-  String respostaDaAlternativaD;*/
+  String respostaDaAlternativaB;
+  String respostaDaAlternativaC;
+  String respostaDaAlternativaD;
 
   bool alternativaA;
-  //bool alternativaB;
-  /*bool alternativaC;
-  bool alternativaD;*/
+  bool alternativaB;
+  bool alternativaC;
+  bool alternativaD;
 
-  Answers({required this.respostaDaAlternativaA, required this.alternativaA});
+  Answers(
+      {required this.respostaDaAlternativaA,
+      required this.alternativaA,
+      required this.respostaDaAlternativaB,
+      required this.alternativaB,
+      required this.respostaDaAlternativaC,
+      required this.alternativaC,
+      required this.respostaDaAlternativaD,
+      required this.alternativaD});
 }
